1c2873ce6c12dbeb13bc9fb0b942ef5f78e0cf67
arfneto-ch <arfneto@hotmail.com>
2019-09-05 23:02:54 -0300

    criada classe Bingo

--- cartela/cartela.cpp
+++ cartela/cartela.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+
 class Cartela
 {
 public:
@@ -18,6 +19,13 @@ public:
 
 	Cartela(string n, int t, int l)
 	{
+		/*
+		Construtor com parâmetros
+
+		Cria cartela para o jogador de nome n, com n numeros e 
+		limite l, e preenche com numeros aleatorios em ordem crescente
+
+		*/
 		nome = n;
 		tamanho = t;
 		limite = l;
@@ -28,7 +36,7 @@ public:
 		int v;
 		// pra vida ficar mais fácil inicia todos os valores
 		// com um valor maior que o limite
-		for (int i=0; i<tamanho; i++) valores[i] = limite+1;
+		for (int i = 0; i < tamanho; i++) valores[i] = limite + 1;
 		srand((int)time(NULL));
 		// aqui vamos preencher a cartela
 		// um loop simples: a cada vez retorna um
@@ -37,15 +45,15 @@ public:
 		for (int i = 0; i < tamanho;)
 		{	// total de valores = tamanho
 			v = rand() % limite + 1; // um valor entre 1 e o limite
-			for (int n=0; n<tamanho; n++)
+			for (int n = 0; n < tamanho; n++)
 			{
 				if (valores[n] > v)
 				{	// achou o lugar: desloca todos pra abrir posicao
-					for (int j=tamanho-1; j>=n; j--)
+					for (int j = tamanho - 1; j >= n; j--)
 					{
-						if(valores[j]<limite+1)
+						if (valores[j] < limite + 1)
 						{
-							valores[j+1] = valores[j];
+							valores[j + 1] = valores[j];
 						}	// end if
 					}	// end for
 					valores[n] = v;
@@ -57,17 +65,78 @@ public:
 		}	// end for i
 	}	// end Cartela()
 
+	Cartela()
+	{
+		nome = "CARTELA";
+		tamanho = 25;
+		limite = 100;
+		//cout << "Criando cartela padrao '" <<
+		//	nome <<
+		//	"' com " << tamanho << " valores entre 1 e " << limite << endl;
+		valores = new int[tamanho];
+		int v;
+		for (int i = 0; i < tamanho; i++) valores[i] = limite + 1;
+		// aqui vamos preencher a cartela
+		// um loop simples: a cada vez retorna um
+		// numero aleatorio. Como na cartela do bingo
+		// os valores ficam em ordem crescente
+		int faixa = 20;
+		int base = 0;
+		for (int i = 0; i < tamanho;)
+		{	// total de valores = tamanho
+			v = rand() % faixa + 1; // um valor entre 1 e o limite da faixa
+			for (int n = 0; n < tamanho; n++)
+			{
+				if (valores[n] > (base + v))
+				{	// achou o lugar: desloca todos pra abrir posicao
+					for (int j = tamanho - 1; j >= n; j--)
+					{
+						if (valores[j] < limite + 1)
+						{
+							valores[j + 1] = valores[j];
+						}	// end if
+					}	// end for
+					valores[n] = base + v;
+					// achei o número n. se completou a faixa avanca
+					if (i % 5 == 4) base += faixa;
+					i++;
+					break;
+				}
+				if (valores[n] == (base +v )) break;
+			}	// end for n
+		}	// end for i
+
+	}	// end Cartela()
+
 
 	~Cartela()
 	{ 
 		cout << 
-			"Destruindo cartela '" << 
+			"Cartela: Destruindo cartela '" << 
 			nome <<
 			"' com " << tamanho <<
 			" valores entre 1 e " << limite
 			<< endl;
 		delete [] valores;
 	};
+
+
+	int existe(unsigned int valor)
+	{
+		for (int i = 0; i < tamanho; i++)
+		{
+			if (valores[i] == valor)
+				return valor;
+		}	// end for
+		return -1;
+	}	// end existe()
+
+
+	static int init()
+	{
+		srand((int)time(NULL));
+		return 0;
+	}	// end init()
 
 
 	int mostra()
@@ -86,24 +155,97 @@ public:
 		return 0;
 	}	// end mostra()
 
-
-	int existe(unsigned int valor)
-	{
-		for (int i=0; i<tamanho; i++)
-		{
-			if (valores[i] == valor)
-				return valor;
-		}	// end for
-		return -1;
-	}	// end existe()
+	void muda_nome(string n) { nome = n; }
 
 };	// end class Cartela
 
+
+class Bingo
+{
+public:
+	string		nome;
+	int			total_cartelas;
+	Cartela*	pCartelas;
+
+	Bingo()
+	{	// construtor padrao assume cinco cartelas
+		Cartela::init();	// inicia o gerador srand()
+		total_cartelas = 5;
+		pCartelas = new (Cartela[total_cartelas]);
+		string		prefixo = "cartela";
+		string		nome = "";
+		for (int i = 1; i <= total_cartelas; i++)
+		{
+			nome = prefixo;
+			nome.append(to_string(i));
+			pCartelas[i - 1].muda_nome(nome);
+		}	// end for
+	}
+
+	Bingo(int n_cartelas)
+	{	// construtor comum para n cartelas
+		Cartela::init();	// inicia o gerador srand()
+		total_cartelas = n_cartelas;
+		pCartelas= new (Cartela[total_cartelas]);
+		string		prefixo = "cartela";
+		string		nome = "";
+		for (int i = 1; i <= total_cartelas; i++)
+		{
+			nome = prefixo;
+			nome.append(to_string(i));
+			pCartelas[i-1].muda_nome(nome);
+		}	// end for
+	}
+
+	~Bingo()
+	{	// ao final libera a memoria
+		cout << "Bingo: Destruindo " << total_cartelas << " cartelas" << endl;
+		delete[] pCartelas;
+	}	// end destructor
+
+
+	void mostra()
+	{
+		cout << "\nBingo: jogo com '" << total_cartelas << "' cartelas" << endl;
+		for (int i = 1; i <= total_cartelas; i++)
+		{
+			cout << i << ": '" <<
+				(pCartelas + i - 1)->nome <<
+				"'" <<
+				endl;
+			//cout << i << ": '" << (pCartelas + i)->nome << "'" << endl;
+		}	// end for
+	}
+
+
+	void mostra_detalhes()
+	{
+		cout << "\nBingo: jogo com '" << total_cartelas << "' cartelas" << endl;
+		for (int i = 1; i <= total_cartelas; i++)
+		{
+			(pCartelas + i - 1)->mostra();
+		}	// end for
+	}
+};	// end class Bingo
+
 int main(int argc, char** argv)
 {
-	Cartela c("Cartela A", 20, 100);
-	c.mostra();
-	Cartela d("Bingo", 10, 50);
-	d.mostra();
+	//Cartela c("Cartela A", 20, 100);
+	//c.mostra();
+	//Cartela d("Bingo", 10, 50);
+	//d.mostra();
+
+	//Cartela comum;
+	//comum.muda_nome("Teste");
+	//comum.mostra();
+
+	cout << "\nCriando uma classe Bingo...\n" << endl;
+	Bingo	bingo;
+	cout << "\nMostra resumo...\n" << endl;
+	bingo.mostra();
+	cout << "\nMostra as cartelas todas...\n" << endl;
+	bingo.mostra_detalhes();
+	cout << "\nMostra a terceira cartela...\n" << endl;
+	(bingo.pCartelas+2)->mostra();
 	return 0;
 }	// end main()
